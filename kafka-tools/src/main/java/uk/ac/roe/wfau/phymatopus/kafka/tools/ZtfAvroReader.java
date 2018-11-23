60bc5f28a15df2b62e768ec73d735cba5ba63b5e
Zarquan <dmr@roe.ac.uk>
2018-11-23 01:02:10 +0000

    Tweaks to Java code

--- kafka-tools/src/main/java/uk/ac/roe/wfau/phymatopus/kafka/tools/ZtfAvroReader.java
+++ kafka-tools/src/main/java/uk/ac/roe/wfau/phymatopus/kafka/tools/ZtfAvroReader.java
@@ -41,7 +41,7 @@ import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.consumer.OffsetAndMetadata; 
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 
 import org.apache.kafka.common.PartitionInfo;
 import org.apache.kafka.common.TopicPartition;
@@ -54,113 +54,30 @@ import ztf.cutout;
 
 
 /**
- * 
- * 
+ *
+ *
  */
 @Slf4j
 public class ZtfAvroReader
 extends BaseReader
 implements ConsumerRebalanceListener
     {
-    
+
     /**
      * Public constructor.
      * @param servers The list of bootstrap Kafka server names.
      * @param group The Kafka client group identifier.
      * @param topic The Kafka topic name.
-     * 
+     *
      */
     public ZtfAvroReader(final String servers, final String group, final String topic)
         {
         super(servers, group, topic);
         }
 
-    /*
-    private String[] schemaNames = {
-        "cutout",
-        "candidate",
-        "prv_candidate",    
-        "alert"
-        };
-
-    private Parser parser = new Parser();
-
-    public void init()
-        {
-        for(String schemaName : schemaNames)
-            {
-            final String schemaFileName = "/" + schemaName + ".avsc" ;
-            log.debug("Openning schema file [{}]", schemaFileName);
-            final InputStream stream = this.getClass().getResourceAsStream(
-                schemaFileName
-                );
-            if (null == stream)
-                {
-                log.error("Unable to find schema file [{}]", schemaFileName);
-                break ;
-                }
-            else {
-                try {
-                    // TODO test out errors we get from bad data ...
-                    log.debug("Parsing schema file [{}]", schemaFileName);
-                    parser.parse(
-                        stream
-                        );
-                    }
-                catch (final IOException ouch)
-                    {
-                    log.error("IOException processing schema file [{}]", schemaFileName);                
-                    break ;
-                    }
-                finally {
-                    try {
-                        stream.close();
-                        }
-                    catch (final IOException ouch)
-                        {
-                        log.error("IOException closing schema file [{}]", schemaFileName);                
-                        }
-                    }
-                }
-            }
-        }
-
-    public Map<String, Schema> schemas()
-        {
-        return this.parser.getTypes();
-        }
-
-    public Schema schema()
-        {
-        return this.parser.getTypes().get(
-            "ztf.alert"
-            );
-        }
-     */
-
-    /**
-     * Create our {@link SchemaRegistryClient}.
-     * 
-    public SchemaRegistryClient registry()
-        {
-        final SchemaRegistryClient registry = new MockSchemaRegistryClient(); 
-        try {
-            registry.register(
-                "ztf.alert",
-                this.schema()
-                );
-            }
-        catch (Exception ouch)
-            {
-            log.error("Exception while registering schema []", ouch.getMessage());
-            }
-        return registry;
-        }
-     */
-    
     /**
      * Create our {@link Consumer}.
-     * 
+     *
      */
     public Consumer<Long, byte[]> consumer()
         {
@@ -199,47 +116,20 @@ implements ConsumerRebalanceListener
             ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
             "earliest"
             );
-        
-        /*
-         * 
-        properties.put(
-            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-            null
-            );
-        properties.put(
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-            KafkaAvroDeserializer.class.getName()
-            );
-         *         
-         */
-        
-        /*
-         * 
-        final Consumer<Object, Object> consumer = new KafkaConsumer<Object, Object>(
-            properties,
-            new KafkaAvroDeserializer(
-                registry()
-                ),
-            new KafkaAvroDeserializer(
-                registry()
-                )
-            );
-         * 
-         */
 
         final KafkaConsumer<Long, byte[]> consumer = new KafkaConsumer<Long, byte[]>(
                 properties,
                 new LongDeserializer(),
                 new ByteArrayDeserializer()
                 );
-        
+
         return consumer;
-        }    
+        }
 
     public void loop(int loops, Duration timeout)
         {
         log.debug("Creating consumer");
-        Consumer<Long, byte[]> consumer = consumer(); 
+        Consumer<Long, byte[]> consumer = consumer();
 
         log.debug("Subscribing ..");
         consumer.subscribe(
@@ -248,60 +138,22 @@ implements ConsumerRebalanceListener
                     ),
                 this
                 );
-        
-        //List<PartitionInfo> partitionsFor(java.lang.String topic)
-        
-        /*
-         * https://stackoverflow.com/a/40017688
-         * 
-        log.debug("First poll ..");
-        ConsumerRecords<Long, byte[]> skip = consumer.poll(
-            Duration.ofSeconds(20)
-            );
-         */ 
 
-        /*
-         * 
-        //
-        // Get the ConsumerCoordinator from the private field of the Consumer:
-        ConsumerCoordinator coordinator = (ConsumerCoordinator) FieldUtils.readField(consumer, "coordinator", true);
-
-        // Force partition assignment.
-        coordinator.ensureActiveGroup();
-        
-        //ensurePartitionAssignment();
-        // Get the list of partitions assigned to this specific consumer:
-        Set<TopicPartition> assigned = consumer.assignment();
-
-        // Now we can call seek(), sequined() or seekToBeginning()) on those partitions only for this consumer as above.
-         * 
-         */
-
-        /*
-        log.debug("Seeking ..");
-        consumer.seekToBeginning(
-            consumer.assignment()
-            );
-        log.debug("Committing ..");
-        consumer.commitSync();
-         */
-        
-        long loopcount = 0 ;
-        long recordcount = 0 ;
+        long bytetotal   = 0 ;
         long recordtotal = 0 ;
-        long bytecount = 0 ;
-        long bytetotal = 0 ;
-        long start = System.nanoTime();
-        long uncommitted = 0 ;
-        
+
         log.debug("Looping ..");
+        long loopcount = 0 ;
         for (int i = 0 ; i < loops ; i++)
             {
             log.debug("Loop [{}]", loopcount++);
+            long pollcount   = 0;
+            long uncommitted = 0;
             do {
-                log.debug("Polling ..");
-                bytecount   = 0;
-                recordcount = 0;
+                log.debug("Poll [{}]", pollcount++);
+                long pollstart = System.nanoTime();
+                long bytecount   = 0;
+                long recordcount = 0;
                 ConsumerRecords<Long, byte[]> records = consumer.poll(
                     timeout
                     );
@@ -326,8 +178,8 @@ implements ConsumerRebalanceListener
                     consumer.commitSync();
                     uncommitted = 0 ;
                     }
-                long nanotime = (System.nanoTime() - start); 
-                log.debug("Loop done [{}] [{}][{}] [{}][{}] in [{}]ns [{}]µs [{}]ms [{}]s => [{}]µs per event",
+                long nanotime = (System.nanoTime() - pollstart);
+                log.debug("Poll done [{}] [{}][{}] [{}][{}] in [{}]ns [{}]µs [{}]ms [{}]s => [{}]µs per event",
                     loopcount,
                     recordcount,
                     recordtotal,
@@ -352,39 +204,55 @@ implements ConsumerRebalanceListener
             }
         }
 
-    
-    /*
-     * Issues -
-     * ZTF Python producer just adds raw Avro, with no schema.
-     * The Confluent deserializer expects schema registry magic bytes at the start of the message.
-     * https://groups.google.com/forum/#!topic/confluent-platform/9LZh3JvnTtI
-     * 
-     * IF we owned the producer, we could add the Schema-Registry bytes to our message.
-     * https://stackoverflow.com/questions/39489649/encoding-formatting-issues-with-python-kafka-library
-     * 
-     * There is a Python Schema-Registry ..
-     * https://github.com/verisign/python-confluent-schemaregistry
-     * 
-     */
-
-    public DataFileReader<alert> reader(final byte[] bytes)
+    public long process(final byte[] bytes)
         {
-        log.debug("Creating alert reader");
-        try {
-            return new DataFileReader<alert>(
-                new SeekableByteArrayInput(
-                    bytes
-                    ),
-                new ReflectDatumReader<alert>(
-                    alert.class
-                    )
-                );             
-            }
-        catch (IOException ouch)
+        log.debug("Hydrating ....");
+        long count = 0 ;
+
+        Schema schema = schema(bytes);
+        final Long fingerprint = SchemaNormalization.parsingFingerprint64(schema);
+        log.debug("Schema fingerprint [{}]", fingerprint);
+
+        DataFileReader<alert> reader = reader(bytes);
+
+        while (reader.hasNext())
             {
-            log.error("IOException creating reader [{}]", ouch.getMessage());
-            return null ;
+            try {
+                log.debug("Hydrating alert [{}]", count++);
+
+                //
+                // Select the implementation type based on the schema fingerprint ...
+                //
+
+                final alert frog = reader.next();
+
+                log.debug("candId    [{}]", frog.getCandid());
+                log.debug("objectId  [{}]", frog.getObjectId());
+                log.debug("schemavsn [{}]", frog.getSchemavsn().toString());
+
+                final cutout science    = frog.getCutoutScience();
+                final cutout template   = frog.getCutoutTemplate();
+                final cutout difference = frog.getCutoutDifference();
+
+                if (null != science)
+                    {
+                    log.debug("science    [{}][{}][{}]", science.getFileName(), science.getStampData().limit(), science.getStampData().capacity());
+                    }
+                if (null != template)
+                    {
+                    log.debug("template   [{}][{}][{}]", template.getFileName(), template.getStampData().limit(), template.getStampData().capacity());
+                    }
+                if (null != difference)
+                    {
+                    log.debug("difference [{}][{}][{}]", difference.getFileName(), difference.getStampData().limit(), difference.getStampData().capacity());
+                    }
+                }
+            catch (RuntimeException ouch)
+                {
+                log.error("RuntimeException hydrating alert [{}]", ouch.getMessage());
+                }
             }
+        return count ;
         }
 
     public Schema schema(final byte[] bytes)
@@ -421,131 +289,31 @@ implements ConsumerRebalanceListener
             }
         return schema;
         }
-    
-    public long process(final byte[] bytes)
+
+    public DataFileReader<alert> reader(final byte[] bytes)
         {
-        log.debug("Hydrating ....");
-        long count = 0 ;
-
-        Schema schema = schema(bytes);
-        final Long fingerprint = SchemaNormalization.parsingFingerprint64(schema);
-        log.debug("Schema fingerprint [{}]", fingerprint);
-        
-        DataFileReader<alert> reader = reader(bytes);
-        
-        while (reader.hasNext())
-            {
-            try {
-                log.debug("Hydrating alert [{}]", count++);
-
-                //
-                // Select the implementation type based on the schema fingerprint ...
-                //
-                
-                final alert frog = reader.next();
-        
-                log.debug("candId    [{}]", frog.getCandid());
-                log.debug("objectId  [{}]", frog.getObjectId());
-                log.debug("schemavsn [{}]", frog.getSchemavsn().toString());
-            
-                final cutout science    = frog.getCutoutScience();
-                final cutout template   = frog.getCutoutTemplate();
-                final cutout difference = frog.getCutoutDifference();
-    
-                if (null != science)
-                    {
-                    log.debug("science    [{}][{}][{}]", science.getFileName(), science.getStampData().limit(), science.getStampData().capacity());
-                    }
-                if (null != template)
-                    {
-                    log.debug("template   [{}][{}][{}]", template.getFileName(), template.getStampData().limit(), template.getStampData().capacity());
-                    }
-                if (null != difference)
-                    {
-                    log.debug("difference [{}][{}][{}]", difference.getFileName(), difference.getStampData().limit(), difference.getStampData().capacity());
-                    }
-                }
-            catch (RuntimeException ouch)
-                {
-                log.error("RuntimeException hydrating alert [{}]", ouch.getMessage());
-                }
+        log.debug("Creating alert reader");
+        try {
+            return new DataFileReader<alert>(
+                new SeekableByteArrayInput(
+                    bytes
+                    ),
+                new ReflectDatumReader<alert>(
+                    alert.class
+                    )
+                );
             }
-        return count ;
-        }
-
-    @Override
-    public void onPartitionsAssigned(Collection<TopicPartition> partitions)
-        {
-        log.debug("PartitionsAssigned()");
-        for (TopicPartition partition : partitions)
+        catch (IOException ouch)
             {
-            log.debug("Partition [{}],[{}]", partition.topic(), partition.partition());
+            log.error("IOException creating reader [{}]", ouch.getMessage());
+            return null ;
             }
         }
 
-    @Override
-    public void onPartitionsRevoked(Collection<TopicPartition> partitions)
-        {
-        log.debug("PartitionsRevoked()");
-        for (TopicPartition partition : partitions)
-            {
-            log.debug("Partition [{}],[{}]", partition.topic(), partition.partition());
-            }
-        }
-
-    public void hexBytes(byte[] bytes)
-        {
-        for (int i = 0 ; i < 0x10 ; i++)
-            {
-            StringBuffer string = new StringBuffer();
-            Formatter formatter = new Formatter(string); 
-            for (int j = 0 ; j < 0x10 ; j++)
-                {
-                int k = (i * 0x10) + j;
-                formatter.format(
-                    "%s%02X",
-                    ((j > 0) ? " " : ""),
-                    bytes[k]
-                    );                
-                }
-            formatter.close();
-            log.debug("Bytes [{}]", string.toString());
-            }
-        }
-    
-    public void asciiBytes(byte[] bytes)
-        {
-        for (int i = 0 ; i < 0x10 ; i++)
-            {
-            StringBuffer string = new StringBuffer();
-            Formatter formatter = new Formatter(string); 
-            for (int j = 0 ; j < 0x10 ; j++)
-                {
-                int k = (i * 0x10) + j;
-                char ascii ;
-                if ((k < bytes.length) && (bytes[k] >= ' ') && (bytes[k] <= '~'))
-                    {
-                    ascii = (char) bytes[k];
-                    }
-                else {
-                    ascii = '.'; 
-                    }
-                formatter.format(
-                    "%s%c",
-                    ((j > 0) ? " " : ""),
-                    ascii
-                    );                
-                }
-            formatter.close();
-            log.debug("ASCII [{}]", string.toString());
-            }
-        }
-    
-    
     public void rewind()
         {
         log.debug("Creating consumer");
-        Consumer<Long, byte[]> consumer = consumer(); 
+        Consumer<Long, byte[]> consumer = consumer();
 
         log.debug("Fetching PartitionInfo for topic");
         List<PartitionInfo> partitions = consumer.partitionsFor(
@@ -582,13 +350,79 @@ implements ConsumerRebalanceListener
                     )
                 );
             }
-        
+
         log.debug("Committing ...");
         consumer.commitSync(offsetmeta);
-        
+
         log.debug("Closing ...");
         consumer.close();
         }
-    
-    
+
+    @Override
+    public void onPartitionsAssigned(Collection<TopicPartition> partitions)
+        {
+        log.debug("PartitionsAssigned()");
+        for (TopicPartition partition : partitions)
+            {
+            log.debug("Partition [{}],[{}]", partition.topic(), partition.partition());
+            }
+        }
+
+    @Override
+    public void onPartitionsRevoked(Collection<TopicPartition> partitions)
+        {
+        log.debug("PartitionsRevoked()");
+        for (TopicPartition partition : partitions)
+            {
+            log.debug("Partition [{}],[{}]", partition.topic(), partition.partition());
+            }
+        }
+
+    public void hexBytes(byte[] bytes)
+        {
+        for (int i = 0 ; i < 0x10 ; i++)
+            {
+            StringBuffer string = new StringBuffer();
+            Formatter formatter = new Formatter(string);
+            for (int j = 0 ; j < 0x10 ; j++)
+                {
+                int k = (i * 0x10) + j;
+                formatter.format(
+                    "%s%02X",
+                    ((j > 0) ? " " : ""),
+                    bytes[k]
+                    );
+                }
+            formatter.close();
+            log.debug("Bytes [{}]", string.toString());
+            }
+        }
+
+    public void asciiBytes(byte[] bytes)
+        {
+        for (int i = 0 ; i < 0x10 ; i++)
+            {
+            StringBuffer string = new StringBuffer();
+            Formatter formatter = new Formatter(string);
+            for (int j = 0 ; j < 0x10 ; j++)
+                {
+                int k = (i * 0x10) + j;
+                char ascii ;
+                if ((k < bytes.length) && (bytes[k] >= ' ') && (bytes[k] <= '~'))
+                    {
+                    ascii = (char) bytes[k];
+                    }
+                else {
+                    ascii = '.';
+                    }
+                formatter.format(
+                    "%s%c",
+                    ((j > 0) ? " " : ""),
+                    ascii
+                    );
+                }
+            formatter.close();
+            log.debug("ASCII [{}]", string.toString());
+            }
+        }
     }
